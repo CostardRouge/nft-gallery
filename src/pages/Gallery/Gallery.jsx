ac4ef8bf76b6894263b65aebb4e85fd3a73b3db9
Steeve Pommier <CostardRouge@users.noreply.github.com>
2022-12-20 16:56:51 +0100

    7b

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,7 +1,17 @@
 import { useEffect, useState, useMemo } from "react";
 
 import "./Gallery.css";
-import { map, sortBy, keyBy,forEach } from "lodash";
+import { map, sortBy } from "lodash";
+
+// https://picsum.photos/512
+
+const getImagePath = (name, path) => {
+  if (import.meta.env.MODE === 'development') {
+    return `https://picsum.photos/seed/${name}/512/512`
+  }
+
+  return `${import.meta.env.BASE_URL}${path}/screenshot.jpeg`
+}
 
 const Gallery = () => {
   const [tree, setTree] = useState({});
@@ -53,8 +63,15 @@ const Gallery = () => {
               {map(sketches, ({ meta: { name }, path }) => (
                 <li key={name} className="sketch">
                   <a href={`${import.meta.env.BASE_URL}${path}`}>
-                    {/* <img src={ `https://picsum.photos/seed/${name}/512/512` }/> */}
-                    <img src={`${import.meta.env.BASE_URL}${path}/screenshot.jpeg`}/>
+                    <a
+                      className="github"
+                      target="_blank"
+                      href={ `https://github.com/CostardRouge/generative-art-p5js/blob/main/${path}/index.js` }
+                    >
+                      source code
+                    </a>
+
+                    <img src={getImagePath(name, path)}/>
                     <span>{name}</span>
                   </a>
                 </li>
